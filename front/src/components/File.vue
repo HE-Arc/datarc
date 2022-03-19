d0d98b71eaca475d9cb2c3716202e26e4781cc61
loic.frossard <loic.frossard@he-arc.ch>
2022-03-19 11:16:41 +0100

    feat<#13> Add Profile Page

---
+++ front/src/components/File.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-white rounded-lg border-0">
+        <div v-on:click="extend" id="test" class="p-2">
+            <h1 class="text-3xl" id="title">{{ name }} {{ id }}</h1>
+            <p>{{ description }}</p>
+            <Like v-on:click="liked" :n="like" />
+        </div>
+        <div id="body">
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            filename: "New File",
+            date: "17.03.2022",
+        };
+    },
+    components: {
+    },
+    methods: {
+    }
+};
+</script>
+
+
+<style >
+</style>
