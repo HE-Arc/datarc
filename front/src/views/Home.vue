4251c2742509f0302e87f3f1943c9dcee82bad89
loic.frossard <loic.frossard@he-arc.ch>
2022-03-19 11:13:27 +0100

    feat<#1> Creation of Home page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,13 +1,41 @@
 <template>
-    <div class="justify-center flex bg-yellow-300 items-center h-screen">
-        <div class="text-4xl">Hello 👋🏼</div>
+    <div class="h-screen">
+        <NavigationBar/>
+        <div class="grid justify-center mx-auto h-5/6 rounded-lg">
+            <div class="flex m-auto gap-2">
+                <div class="flex-col ">
+                    <div class="title text-6xl text-yellow-400 mb-4">
+                        Welcome
+                    </div>
+                    <p class="text-justify text-xl text-white">
+                        {{ welcomeText }}
+                    </p>
+                </div>
+                <div class="grid justify-center content-end w-1/3 p-2">
+                    <router-link to="login" class="btn text-xl">Login</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import NavigationBar from '@/components/NavigationBar';
+
 export default {
     name: "Home",
-    components: {},
-    method: {},
+    data() {
+        return{
+            welcomeText: 'Stocker et partager vos fichiers et dossier, entre vos appareils ou vos amis',
+        }
+    },
+    methods: {},
+    components: {
+        NavigationBar,
+    },
 };
 </script>
+
+<style>
+
+</style>
