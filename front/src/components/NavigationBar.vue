f47fe8040cc833e82b476eeb816ad91ca901b3ba
petitBiscuits <loic.frossard01@gmail.com>
2022-04-11 10:20:40 +0200

    style<#2> changing some color

--- front/src/components/NavigationBar.vue
+++ front/src/components/NavigationBar.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="w-full dark:bg-blue-500">
+    <div class="w-full bg-white-background-header dark:bg-dark-background-header">
         <div class="w-3/4 container flex justify-between items-center mx-auto">
-            <div class="">
-                <div class="cursor-pointer" @click="toggleDarkMode">
-                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="48px" viewBox="0 0 20 20" width="48px" fill="#000000" v-if="!this.darkMode">
+            <div class="w-1/3">
+                <div class="inline-block cursor-pointer text-yellow-300" @click="toggleDarkMode">
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="48px" viewBox="0 0 20 20" width="48px" fill="currentColor" v-if="!this.darkMode">
                         <rect fill="none" height="20" width="20"/>
                         <path d="M10,7.5c1.38,0,2.5,1.12,2.5,2.5s-1.12,2.5-2.5,2.5S7.5,11.38,7.5,10S8.62,7.5,10,7.5z M10,6c-2.21,0-4,1.79-4,4s1.79,4,4,4 s4-1.79,4-4S12.21,6,10,6L10,6z M3.75,10.75c0.41,0,0.75-0.34,0.75-0.75c0-0.41-0.34-0.75-0.75-0.75h-2C1.34,9.25,1,9.59,1,10 s0.34,0.75,0.75,0.75H3.75z M18.25,10.75c0.41,0,0.75-0.34,0.75-0.75c0-0.41-0.34-0.75-0.75-0.75h-2c-0.41,0-0.75,0.34-0.75,0.75 s0.34,0.75,0.75,0.75H18.25z M9.25,3.75C9.25,4.16,9.59,4.5,10,4.5c0.41,0,0.75-0.34,0.75-0.75v-2C10.75,1.34,10.41,1,10,1 S9.25,1.34,9.25,1.75V3.75z M13.89,5.05c-0.29,0.29-0.29,0.77,0,1.06s0.77,0.29,1.06,0l1.06-1.06c0.29-0.29,0.29-0.77,0-1.06 c-0.29-0.29-0.77-0.29-1.06,0L13.89,5.05z M3.99,14.95c-0.29,0.29-0.29,0.77,0,1.06s0.77,0.29,1.06,0l1.06-1.06 c0.29-0.29,0.29-0.77,0-1.06c-0.29-0.29-0.77-0.29-1.06,0L3.99,14.95z M5.05,6.11c0.29,0.29,0.77,0.29,1.06,0s0.29-0.77,0-1.06 L5.05,3.99c-0.29-0.29-0.77-0.29-1.06,0s-0.29,0.77,0,1.06L5.05,6.11z M14.95,16.01c0.29,0.29,0.77,0.29,1.06,0s0.29-0.77,0-1.06 l-1.06-1.06c-0.29-0.29-0.77-0.29-1.06,0c-0.29,0.29-0.29,0.77,0,1.06L14.95,16.01z M9.25,18.25C9.25,18.66,9.59,19,10,19 c0.41,0,0.75-0.34,0.75-0.75v-2c0-0.41-0.34-0.75-0.75-0.75s-0.75,0.34-0.75,0.75V18.25z"/>
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="48px" viewBox="0 0 20 20" width="48px" fill="#000000" v-if="this.darkMode">
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="48px" viewBox="0 0 20 20" width="48px" fill="currentColor" v-if="this.darkMode">
                         <g>
                             <rect fill="none" height="20" width="20" x="0"/>
                         </g>
@@ -21,11 +21,11 @@
                 </div>
             </div>
 
-            <div class="logo text-yellow-300 font-bold text-4xl py-4">
+            <div class="w-1/3 text-center logo text-yellow-300 text-5xl font-bold py-4">
                 Datarc
             </div>
-            <div class="">
-                <ul class="flex flex-row mt-4 md:mt-0">
+            <div class="w-1/3">
+                <ul class="flex flex-row justify-end mt-4 md:mt-0">
                     <li>
                         <router-link to="register" class="btn btn-yellow bg-transparent rounded-r-none">Sign up</router-link>
                     </li>
@@ -52,11 +52,21 @@ export default {
         toggleDarkMode(){
             document.documentElement.classList.toggle('dark');
             this.darkMode = !this.darkMode;
+            if(this.darkMode){
+                localStorage.theme = 'dark';
+            }else{
+                localStorage.theme = 'light';
+            }
         },
     },
     
     components: {},
     mounted(){
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)){
+            document.documentElement.classList.add('dark');
+        }else{
+            document.documentElement.classList.remove('dark');
+        }
         this.darkMode = document.documentElement.classList.contains('dark');
     }
 };
