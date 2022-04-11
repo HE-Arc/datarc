dedf3de792ae13603855fda1d5ecb6bfc05c94cf
petitBiscuits <loic.frossard01@gmail.com>
2022-04-11 09:12:50 +0200

    style<#13> changing style

--- front/src/components/File.vue
+++ front/src/components/File.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="bg-gray-200 max-w-full min-w-0 cursor-pointer rounded-lg m-4 z-20" @click="openModal" @keydown.esc="closeModal" tabindex="0">
-        <div class="w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <div class="bg-white cursor-pointer rounded-lg m-4 z-20" @click="openModal" @keydown.esc="closeModal" tabindex="0">
+        <div class="w-full h-full relative">
+            <svg class="h-5/6 text-black" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             </svg>
             
-            <div class="w-full rounded-b-lg bg-red-400 opacity-70">
-                <div class="w-11/12 font-bold overflow-hidden overflow-ellipsis mx-auto whitespace-nowrap backdrop-opacity-100">
-                    {{ filename }}
+            <div class="w-full h-1/6 rounded-b-lg bg-red-500 opacity-70">
+                <div class="w-11/12 h-full font-bold overflow-hidden overflow-ellipsis mx-auto whitespace-nowrap blur-3xl">
+                    {{ name }}
                 </div>
             </div>
         </div>
     </div>
-    <Modal :isShow="isSelected" @close-modal="closeModal"/>
+    <Modal :isShow="isSelected" @close-modal="closeModal" :name="name" :date="date" :author="author"/>
 </template>
 
 <script>
@@ -22,11 +22,11 @@ import Modal from "@/components/Modal";
 export default {
     props: {
         name : String,
+        date : String,
+        author : String,
     },
     data() {
         return {
-            filename: "New File",
-            date: "17.03.2022",
             isSelected : false,
         };
     },
@@ -50,10 +50,6 @@ export default {
             }
         }
         
-    },
-    mounted() {
-        if (this.name != null)
-            this.filename = this.name;
     },
 };
 </script>
--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -2,14 +2,15 @@
     <div class="h-screen">
         <div class="flex flex-col h-full w-full mx-auto rounded-lg">
             <NavigationBar/>
-            <div class="mx-auto">
+            <div class="w-11/12 mx-auto text-center text-2xl p-7 font-bold border-b-2 border-black dark:border-dark-border-1">
                 Mon Profile
             </div>
-            <div class="flex gap-4 h-full mx-8 mt-8">
+            <div class="flex gap-4 mx-8 mt-8 relative">
                 <div class="w-1/2 h-full mx-auto relative">
-                <UploadWindow/>
-                    <div class="flex justify-between bg-gray-700">
-                        <div class="text-3xl font-bold text-center p-4">Mes Fichiers</div>
+                    <div class="flex min-h-min justify-between bg-white-background-subtitle dark:bg-dark-background-subtitle rounded-md sticky top-0 z-50">
+                        <div class="text-3xl font-bold text-center p-4 ">
+                            Mes Fichiers
+                        </div>
                         <input type="file" id="file" hidden/>
                         <label for="file" class="w-16 p-2 text-yellow-300 cursor-pointer z-40">
                             <svg @click="upload" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -17,18 +18,17 @@
                             </svg>
                         </label>
                     </div>
-                    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                        <File name="Salut daslikjdasolkdkj ojdal jslda"/>
-                        <File/>
+                    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 overflow-x-auto relative">
+                        <UploadWindow/>
+                        <File v-for="item in items" :key="item" :name="item.name" :date="item.date" :author="item.author"/>
                     </div>
                 </div>
                 <div class="w-1/2 mx-auto">
-                    <div class="flex bg-gray-700">
+                    <div class="flex bg-white-background-subtitle dark:bg-dark-background-subtitle rounded-md sticky top-0 z-50">
                         <div class="text-3xl font-bold text-center p-4">Mes fichiers partagés avec moi</div>
                     </div>
-                    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                        <File/>
-                        <File/>
+                    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 overflow-x-auto relative">
+                        <File v-for="item in itemsOther" :key="item" :name="item.name" :date="item.date" :author="item.author"/>
                     </div>
                 </div> 
             </div> 
@@ -43,6 +43,49 @@ import File from '@/components/File';
 
 export default {
     name: "Profile",
+    data() {
+        return {
+            items: [
+                {
+                    name : 'test',
+                    date : '02.04.2022',
+                    author : 'Loïc'
+                },
+                {
+                    name : 'test1',
+                    date : '02.04.2022',
+                    author : 'Loïc'
+                },
+                {
+                    name : 'test2',
+                    date : '02.04.2022',
+                    author : 'Loïc'
+                },
+                {
+                    name : 'test3',
+                    date : '02.04.2022',
+                    author : 'Loïc'
+                },
+                {
+                    name : 'test4dajkjdna lkndaslkd aslidjasoi jdaosijd asoijd oiasjdoasij oisajd oiasjasoidj asoidja',
+                    date : '02.04.2022',
+                    author : 'Loïc'
+                },
+            ],
+            itemsOther: [
+                {
+                    name : 'mathieu.xlsx',
+                    date : '02.04.2022',
+                    author : 'Mathieu'
+                },
+                {
+                    name : 'thibault.pdf',
+                    date : '02.04.2022',
+                    author : 'Thibault'
+                },
+            ],
+        };
+    },
     methods: {
     },
     components: {
