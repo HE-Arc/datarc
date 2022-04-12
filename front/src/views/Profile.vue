b5bfddcf97d572cb35c394e08f46fd87d26dc31a
petitBiscuits <loic.frossard01@gmail.com>
2022-04-12 18:58:32 +0200

    fix: upload

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -44,7 +44,7 @@
 	import File from "@/components/File";
 	import { getData } from "../Tools/Network.js";
 	import { getCookie } from "../Tools/Cookie.js";
-	import { goTo } from "../Tools/nav.js";
+	//import { goTo } from "../Tools/nav.js";
 	import { uploadFile } from "../Tools/UploadTools.js";
 
 	export default {
@@ -71,7 +71,7 @@
 		methods: {
 			upload() {
 				console.log("upload !");
-				const selectedFile = document.getElementById("input1").files[0];
+				const selectedFile = document.getElementById("file").files[0];
 				if (selectedFile == null) {
 					this.error("no file selected");
 					return;
@@ -111,7 +111,7 @@
 						this.name = data.name;
 					}
 				} catch (error) {
-					goTo("/");
+					//goTo("/");
 				}
 				try {
 					let data = await getData("/files", {
@@ -128,10 +128,10 @@
 						}
 					}
 				} catch (error) {
-					goTo("/");
+					//goTo("/");
 				}
 			} else {
-				goTo("/");
+				//goTo("/");
 			}
 		},
 	};
