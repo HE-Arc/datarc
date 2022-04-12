08f5102b8a7352d7aa74804f3c57863e46968ad4
matthieu.barbot <matthieu.barbot@he-arc.ch>
2022-04-12 02:47:17 +0200

    connect upload and download

--- front/src/components/Modal.vue
+++ front/src/components/Modal.vue
@@ -1,13 +1,72 @@
 <template>
     <Transition name="fade">
-        <div class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center text-black z-30" v-if="isShow">
-            <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
-        
-            <div class="modal-container bg-white w-11/12 md:max-w-md lg:max-w-lg mx-auto rounded shadow-lg z-50 overflow-y-auto">
-        
-                <div class="absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-                    <svg @click="closeModal" class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+        <div
+            class="
+                ez
+                modal
+                fixed
+                w-full
+                h-full
+                top-0
+                left-0
+                flex
+                items-center
+                justify-center
+                text-black
+                z-30
+            "
+            v-if="isShow"
+        >
+            <div
+                class="
+                    modal-overlay
+                    absolute
+                    w-full
+                    h-full
+                    bg-gray-900
+                    opacity-50
+                "
+            ></div>
+
+            <div
+                class="
+                    modal-container
+                    bg-white
+                    w-11/12
+                    md:max-w-md
+                    lg:max-w-lg
+                    mx-auto
+                    rounded
+                    shadow-lg
+                    z-50
+                    overflow-y-auto
+                "
+            >
+                <div
+                    class="
+                        absolute
+                        top-0
+                        right-0
+                        cursor-pointer
+                        flex flex-col
+                        items-center
+                        mt-4
+                        mr-4
+                        text-white text-sm
+                        z-50
+                    "
+                >
+                    <svg
+                        @click="closeModal"
+                        class="fill-current text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                    >
+                        <path
+                            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                        ></path>
                     </svg>
                     <span class="text-sm">(Esc)</span>
                 </div>
@@ -18,8 +77,16 @@
                     <div class="flex justify-between items-center pb-3">
                         <p class="text-2xl font-bold">{{ name }}</p>
                         <div @click="closeModal" class="cursor-pointer z-50">
-                            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            <svg
+                                class="fill-current text-black"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                            >
+                                <path
+                                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                                ></path>
                             </svg>
                         </div>
                     </div>
@@ -31,60 +98,192 @@
 
                     <!--Footer-->
                     <div class="flex justify-end pt-2">
-                        <button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
-                        <button @click="closeModal" class="px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+                        <button
+                            class="
+                                px-4
+                                bg-transparent
+                                p-3
+                                rounded-lg
+                                text-indigo-500
+                                hover:bg-gray-100 hover:text-indigo-400
+                                mr-2
+                            "
+                            @click="copy"
+                        >
+                            copy link
+                        </button>
+
+                        <button
+                            class="
+                                px-4
+                                bg-transparent
+                                p-3
+                                rounded-lg
+                                text-indigo-500
+                                hover:bg-gray-100 hover:text-indigo-400
+                                mr-2
+                            "
+                            @click="download"
+                        >
+                            download
+                        </button>
+                        <button
+                            @click="closeModal"
+                            class="
+                                px-4
+                                bg-indigo-500
+                                p-3
+                                rounded-lg
+                                text-white
+                                hover:bg-indigo-400
+                            "
+                        >
+                            Close
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     </Transition>
+    <div id="snackbar">link copied to clipboard...</div>
 </template>
 <script>
+import { downloadFile, getLink } from "../Tools/UploadTools.js";
+import { getCookie } from "../Tools/Cookie.js";
 
 export default {
     props: {
-        isShow : Boolean,
-        name : String,
-        date : String,
-        author : String,
+        isShow: Boolean,
+        name: String,
+        date: String,
+        author: String,
+        url: String,
     },
     data() {
         return {
-            formatFile : "inconu",
+            formatFile: "inconu",
         };
     },
-    components: {
-    },
+    components: {},
     watch: {
-        name: function(){
+        name: function () {
             let temp = this.name.split(".");
-            if(temp.length() > 1){
-                this.formatFile = temp[temp.length()-1];
+            if (temp.length() > 1) {
+                this.formatFile = temp[temp.length() - 1];
             }
-        }
+        },
     },
     methods: {
-        closeModal(){
-            this.$emit('close-modal')
-        }
+        closeModal() {
+            this.$emit("close-modal");
+        },
+        async download() {
+            let token = getCookie("token");
+            console.log(this.url);
+            try {
+                await downloadFile(this.url, token, this.name);
+            } catch (e) {
+                this.error("error while downloading the file");
+            }
+        },
+        copy() {
+            navigator.clipboard.writeText(getLink(this.url));
+            var x = document.getElementById("snackbar");
+            x.className = "show";
+            setTimeout(function () {
+                x.className = x.className.replace("show", "");
+            }, 3000);
+        },
+        error(msg) {
+            console.log(msg);
+        },
     },
     created() {
         let temp = this.name.split(".");
-        if(temp.length > 1){
-            this.formatFile = "." + temp[temp.length-1];
+        if (temp.length > 1) {
+            this.formatFile = "." + temp[temp.length - 1];
         }
-    }
+    },
 };
 </script>
 <style >
+.ez {
+    z-index: 999;
+}
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.25s ease;
+    transition: opacity 0.25s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
+}
+
+#snackbar {
+    visibility: hidden;
+    min-width: 250px;
+    margin-left: -125px;
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    border-radius: 2px;
+    padding: 16px;
+    position: fixed;
+    z-index: 1;
+    left: 50%;
+    bottom: 30px;
+    font-size: 17px;
+}
+
+#snackbar.show {
+    visibility: visible;
+    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+    animation: fadein 0.5s, fadeout 0.5s 2.5s;
+}
+
+@-webkit-keyframes fadein {
+    from {
+        bottom: 0;
+        opacity: 0;
+    }
+    to {
+        bottom: 30px;
+        opacity: 1;
+    }
+}
+
+@keyframes fadein {
+    from {
+        bottom: 0;
+        opacity: 0;
+    }
+    to {
+        bottom: 30px;
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes fadeout {
+    from {
+        bottom: 30px;
+        opacity: 1;
+    }
+    to {
+        bottom: 0;
+        opacity: 0;
+    }
+}
+
+@keyframes fadeout {
+    from {
+        bottom: 30px;
+        opacity: 1;
+    }
+    to {
+        bottom: 0;
+        opacity: 0;
+    }
 }
 </style>
--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -1,101 +1,200 @@
 <template>
     <div class="h-screen">
         <div class="flex flex-col h-full w-full mx-auto rounded-lg">
-            <NavigationBar/>
-            <div class="w-11/12 mx-auto text-center text-2xl p-7 font-bold border-b-2 border-black dark:border-dark-border-1">
+            <NavigationBar />
+            <div
+                class="
+                    w-11/12
+                    mx-auto
+                    text-center text-2xl
+                    p-7
+                    font-bold
+                    border-b-2 border-black
+                    dark:border-dark-border-1
+                "
+            >
                 Mon Profile
             </div>
             <div class="flex gap-4 mx-8 mt-8 relative">
                 <div class="w-1/2 h-full mx-auto relative">
-                    <div class="flex min-h-min justify-between bg-white-background-subtitle dark:bg-dark-background-subtitle rounded-md sticky top-0 z-50">
-                        <div class="text-3xl font-bold text-center p-4 ">
+                    <div
+                        class="
+                            flex
+                            min-h-min
+                            justify-between
+                            bg-white-background-subtitle
+                            dark:bg-dark-background-subtitle
+                            rounded-md
+                            sticky
+                            top-0
+                        "
+                    >
+                        <div class="text-3xl font-bold text-center p-4">
                             Mes Fichiers
                         </div>
-                        <input type="file" id="file" hidden/>
-                        <label for="file" class="w-16 p-2 text-yellow-300 cursor-pointer z-40">
-                            <svg @click="upload" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-                            </svg>
+                        <label
+                            for="file"
+                            class="w-16 p-2 text-yellow-300 cursor-pointer"
+                        >
+                            <input type="file" id="input1" required />
                         </label>
+                        <button v-on:click="upload">Envoyer</button>
                     </div>
-                    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 overflow-x-auto relative">
-                        <UploadWindow/>
-                        <File v-for="item in items" :key="item" :name="item.name" :date="item.date" :author="item.author"/>
+                    <div
+                        class="
+                            grid
+                            md:grid-cols-2
+                            lg:grid-cols-3
+                            xl:grid-cols-4
+                            overflow-x-auto
+                            relative
+                        "
+                    >
+                        <UploadWindow />
+                        <File
+                            v-for="item in items"
+                            :key="item"
+                            :name="item.name"
+                            :date="item.date"
+                            :author="item.author"
+                            :url="item.url"
+                        />
                     </div>
                 </div>
                 <div class="w-1/2 mx-auto">
-                    <div class="flex bg-white-background-subtitle dark:bg-dark-background-subtitle rounded-md sticky top-0 z-50">
-                        <div class="text-3xl font-bold text-center p-4">Mes fichiers partagés avec moi</div>
+                    <div
+                        class="
+                            flex
+                            bg-white-background-subtitle
+                            dark:bg-dark-background-subtitle
+                            rounded-md
+                            sticky
+                            top-0
+                        "
+                    >
+                        <div class="text-3xl font-bold text-center p-4">
+                            Mes fichiers partagés avec moi
+                        </div>
                     </div>
-                    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 overflow-x-auto relative">
-                        <File v-for="item in itemsOther" :key="item" :name="item.name" :date="item.date" :author="item.author"/>
+                    <div
+                        class="
+                            grid
+                            md:grid-cols-2
+                            lg:grid-cols-3
+                            xl:grid-cols-4
+                            overflow-x-auto
+                            relative
+                        "
+                    >
+                        <File
+                            v-for="item in itemsOther"
+                            :key="item"
+                            :name="item.name"
+                            :date="item.date"
+                            :author="item.author"
+                            :url="item.url"
+                        />
                     </div>
-                </div> 
-            </div> 
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import NavigationBar from '@/components/NavigationBar';
-import UploadWindow from '@/components/UploadWindow';
-import File from '@/components/File';
+import NavigationBar from "@/components/NavigationBar";
+import UploadWindow from "@/components/UploadWindow";
+import File from "@/components/File";
+import { getData } from "../Tools/Network.js";
+import { getCookie } from "../Tools/Cookie.js";
+import { goTo } from "../Tools/nav.js";
+import { uploadFile } from "../Tools/UploadTools.js";
 
 export default {
     name: "Profile",
     data() {
         return {
-            items: [
-                {
-                    name : 'test',
-                    date : '02.04.2022',
-                    author : 'Loïc'
-                },
-                {
-                    name : 'test1',
-                    date : '02.04.2022',
-                    author : 'Loïc'
-                },
-                {
-                    name : 'test2',
-                    date : '02.04.2022',
-                    author : 'Loïc'
-                },
-                {
-                    name : 'test3',
-                    date : '02.04.2022',
-                    author : 'Loïc'
-                },
-                {
-                    name : 'test4dajkjdna lkndaslkd aslidjasoi jdaosijd asoijd oiasjdoasij oisajd oiasjasoidj asoidja',
-                    date : '02.04.2022',
-                    author : 'Loïc'
-                },
-            ],
+            items: [],
             itemsOther: [
                 {
-                    name : 'mathieu.xlsx',
-                    date : '02.04.2022',
-                    author : 'Mathieu'
+                    name: "mathieu.xlsx",
+                    date: "02.04.2022",
+                    author: "Mathieu",
+                    url: "aa",
                 },
                 {
-                    name : 'thibault.pdf',
-                    date : '02.04.2022',
-                    author : 'Thibault'
+                    name: "thibault.pdf",
+                    date: "02.04.2022",
+                    author: "Thibault",
+                    url: "aa",
                 },
             ],
         };
     },
     methods: {
+        upload() {
+            console.log("upload !");
+            const selectedFile = document.getElementById("input1").files[0];
+            if (selectedFile == null) {
+                this.error("no file selected");
+                return;
+            }
+            if (selectedFile.size > 2000000) {
+                this.error("the file selected is too big !");
+                return;
+            }
+            let token = getCookie("token");
+            if (token != "") {
+                try {
+                    console.log("ahahahaha");
+                    uploadFile(selectedFile, token);
+                } catch (e) {
+                    this.error(e);
+                }
+            }
+        },
+        error(msg) {
+            console.log(msg);
+        },
     },
     components: {
         NavigationBar,
         UploadWindow,
         File,
     },
+    async created() {
+        let token = getCookie("token");
+        if (token != "") {
+            try {
+                let data = await getData("/user", { Authentication: token });
+                if (data.status == "ok") {
+                    this.isConnected = true;
+                    this.name = data.name;
+                }
+            } catch (error) {
+                goTo("/");
+            }
+            try {
+                let data = await getData("/files", { Authentication: token });
+                if (data.status == "ok") {
+                    for (let i = 0; i < data.files.length; i++) {
+                        this.items.push({
+                            name: data.files[i].name,
+                            data: "",
+                            author: "me",
+                            url: data.files[i].url,
+                        });
+                    }
+                }
+            } catch (error) {
+                goTo("/");
+            }
+        } else {
+            goTo("/");
+        }
+    },
 };
 </script>
 
 <style>
-
 </style>
