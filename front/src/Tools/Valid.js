37cd03c3ef498422243095ad342653672c68f71f
Thibault Wermeille <thibault.wermeille@he-arc.ch>
2022-04-12 16:24:23 +0200

    change mail to accept usernames

--- front/src/Tools/Valid.js
+++ front/src/Tools/Valid.js
@@ -3,8 +3,9 @@ function validPassword(password) {
     return re.test(password);
 }
 
+// todo rename into Username
 function validMail(mail) {
-    const re = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    const re = /^[A-Za-z0-9]{2,24}/;
     return re.test(mail);
 }
 
